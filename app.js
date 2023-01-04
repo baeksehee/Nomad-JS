b2cd0944a113afba801ba70c2ae498a019d71663
baeksehee <tpgmlqor@gmail.com>
2023-01-04 10:15:09 +0900

    4.1 Form Submission

--- app.js
+++ app.js
@@ -3,11 +3,7 @@ const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick() {
   const username = loginInput.value;
-  if (username === "") {
-    alert("Please write your name");
-  } else if(username.length > 15) {
-    alert("Your name is too long.")
-  }
+  console.log(username);
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);

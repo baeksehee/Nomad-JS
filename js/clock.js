e84be1a26440f5539a7c66ca5f513cb86f1686fb
baeksehee <tpgmlqor@gmail.com>
2023-01-08 23:40:04 +0900

    css파일 href 파일 위치  오류 수정

---
+++ js/clock.js
@@ -0,0 +1,2 @@
+const clock = document.querySelector("h2#clock"); //("clock")만 해도 괜찮다.
+
--- js/greetings.js
+++ js/greetings.js
@@ -4,19 +4,16 @@ const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
-//변수명이 오타가 나면, 자바스크립트는 지적해준다. ex)USERNAME_KEY
-//만약 string을 작성하다가 오타가 나면 자바스크립트는 이걸 지적하지 않을 것이다. ex)"username"
 
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
-  paintGreetings();
+  paintGreetings(username);
 }
 
-function paintGreetings() {
-  const username = localStorage.getItem(USERNAME_KEY);
+function paintGreetings(username) {
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
@@ -27,5 +24,5 @@ if (savedUsername == null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-  paintGreetings();
+  paintGreetings(savedUsername);
 }

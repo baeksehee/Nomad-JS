b128004bc15b820ea63be978a65900c516277790
baeksehee <tpgmlqor@gmail.com>
2023-01-08 20:55:15 +0900

    4.7 Super Recap (JS 흐름이 중요한 듯, 또반복되는 코드를 줄이는 것도 중요한 듯)

--- app.js
+++ app.js
@@ -12,10 +12,11 @@ function onLoginSubmit(event) {
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
-  paintGreetings(username);
+  paintGreetings();
 }
 
-function paintGreetings(username) {
+function paintGreetings() {
+  const username = localStorage.getItem(USERNAME_KEY);
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
@@ -26,5 +27,5 @@ if (savedUsername == null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-  paintGreetings(savedUsername);
+  paintGreetings();
 }

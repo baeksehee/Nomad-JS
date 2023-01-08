b2cb2a3ab5d6c78393db438f585b460c6fba6e30
baeksehee <tpgmlqor@gmail.com>
2023-01-08 19:35:13 +0900

    4.6 Loading Username(반복되는 string 뱐수 설정, JS 중 function 만들기와 if문 생성)

--- app.js
+++ app.js
@@ -3,16 +3,28 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+//변수명이 오타가 나면, 자바스크립트는 지적해준다. ex)USERNAME_KEY
+//만약 string을 작성하다가 오타가 나면 자바스크립트는 이걸 지적하지 않을 것이다. ex)"username"
 
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  localStorage.setItem("username", username);//우선 저장될 값의
-  //이름(key)를 저장해주고, 그 다음에는 값인 username을 적어주면 된다.
-  //("저장될 아이템의 이름", username 변수)
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+}
+
+function paintGreetings(username) {
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername == null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}

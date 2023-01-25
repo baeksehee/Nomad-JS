5eb8356ae113f4bc883b4dd0e0216afe9740b062
baeksehee <tpgmlqor@gmail.com>
2023-01-25 20:12:26 +0900

    7.2 Deleting To Dos(evet, target, parentElement)

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,20 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input"); // input을 document가 아닌 toDoForm 안에서 찾는 것이다.
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event) {
+  const li = event.target.parentElement; //target은 클릭된 HTML element.
+  li.remove();
+}
+
 function paintToDo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span);
   span.innerText = newTodo; //span의 텍스트는 handleToDoSubmit에서 온 newTodo 텍스트가 된다.
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
   toDoList.appendChild(li);
 }
 
